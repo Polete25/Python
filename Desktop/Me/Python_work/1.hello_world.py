38469dd855365a718d1ed370e10d6940d5ed7946
Pol Alomar <polalomar25@gmail.com>
2024-02-20 20:05:59 +0800

    Update 1.hello_world.py

--- Desktop/Me/Python_work/1.hello_world.py
+++ Desktop/Me/Python_work/1.hello_world.py
@@ -1,7 +1,7 @@
-# print("Hello Python world!")
+print("Hello Python world!")
 
-message = "Hello Python world!"
-print(message)
-message = "Hello Python Crash Course world!"
-print(message)
+#message = "Hello Python world!"
+#print(message)
+#message = "Hello Python Crash Course world!"
+#print(message)
 
